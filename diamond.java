957482523d49fd81f6e37b120dc722d662b18797
anaghasethu <44130212+anaghasethu@users.noreply.github.com>
2019-02-05 22:38:58 +0530

    Add files via upload

---
+++ diamond.java
@@ -0,0 +1,60 @@
+
+class diamond 
+{
+public static void main(String args[])
+{
+
+int i,j=0;
+
+for(i=0;i<3;i++)
+	{
+		for(j=0;j<20;j++)
+		{
+			System.out.print("1");
+		}
+		System.out.println("");
+	}
+
+for(i=0;i<6;i++)
+	{
+	for(j=0;j<4;j++)
+		{
+			System.out.print("1");
+		}
+	System.out.println("");
+	
+	}
+
+for(i=0;i<3;i++)
+	{
+		for(j=0;j<20;j++)
+		{
+			System.out.print("1");
+		}
+		System.out.println("");
+	}
+
+for(i=0;i<6;i++)
+	{
+	for(j=0;j<4;j++)
+		{
+			System.out.print("1");
+		}
+	System.out.println("");
+	
+	}
+
+for(i=0;i<3;i++)
+	{
+		for(j=0;j<20;j++)
+		{
+			System.out.print("1");
+		}
+		System.out.println("");
+	}
+
+
+
+}
+}
+
